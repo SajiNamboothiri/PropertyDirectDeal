cf6c379f704bd399c6d76b6e3a2106b96092df35
SajiNamboothiri <saji.euro@gmail.com>
2020-09-26 05:56:13 +0530

    Made an unnecessary copy

---
+++ connection - Copy copy.php
@@ -0,0 +1,28 @@
+<?php
+
+$username="u826618413_property1";
+$password="Saji1234";
+$server='localhost';
+$db='u826618413_property1';
+$con=mysqli_connect($server,$username,$password,$db);
+/*this is a copy*/
+
+/*if($con){
+    //echo"Connection to Database Successful";
+    
+?>
+<script>
+    alert('connection successful')
+</script>
+
+<?php
+
+
+   
+}
+else{
+    echo"No connection to Database";
+}*/
+
+
+?>
